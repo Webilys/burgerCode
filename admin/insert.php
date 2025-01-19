4f9cb538332eb9987adeaf24f796e856057302cb
Priscilla DigiTech <priscilladigitech@gmail.com>
2025-01-19 11:15:18 +0100

    19/01/2025 11:15

--- admin/insert.php
+++ admin/insert.php
@@ -90,13 +90,16 @@ function checkInput($data)
 <html>
 
 <head>
-    <title>Burger Code</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Burger Code</title>
+    <link rel="icon" href="../images/burger-code-icon.png">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Holtwood+One+SC&display=swap');
+    </style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
 
